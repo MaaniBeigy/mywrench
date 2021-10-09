702a781c3e50610bfaf50c8eb9f9d854661283b6
smrrazavian <smrrazavian@outlook.com>
2021-10-09 13:02:30 +0330

    Plots Are Deleted

--- python/k-means-ex.py
+++ python/k-means-ex.py
@@ -106,37 +106,3 @@ clustering_metrics = [
     metrics.completeness_score(x, labels),
     metrics.silhouette_score(x, labels)
 ]
-
-plt.scatter(
-    x[y_km == 0, 0], x[y_km == 0, 1],
-    s=50, c='lightgreen',
-    marker='s', edgecolor='black',
-    label='cluster 1'
-)
-
-plt.scatter(
-    x[y_km == 1, 0], x[y_km == 1, 1],
-    s=50, c='orange',
-    marker='o', edgecolor='black',
-    label='cluster 2'
-)
-
-
-plt.scatter(
-    x[y_km == 2, 0], x[y_km == 2, 1],
-    s=50, c='lightblue',
-    marker='v', edgecolor='black',
-    label='cluster 3'
-)
-# plot the centroids
-
-plt.scatter(
-    km_main.cluster_centers_[:, 0], km_main.cluster_centers_[:, 1],
-    s=250, marker='*',
-    c='red', edgecolor='black',
-    label='centroids'
-)
-plt.legend(scatterpoints=1)
-plt.grid()
-plt.show()
-
