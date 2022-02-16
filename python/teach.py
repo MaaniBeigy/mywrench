8e10798385d58bd1c4ac67a1ced2a8b28123b378
MaaniBeigy <manibeygi@gmail.com>
2022-02-16 08:12:40 +0330

    :lipstick: editing lint errors

--- python/teach.py
+++ python/teach.py
@@ -1,10 +1,14 @@
-# Statistical Programming in Python
-from matplotlib import pyplot as plt  # Visualization with Python
-import seaborn as sns  # Statistical data visualization
-import numpy as np  # Fundamental package for scientific computing with Python
-import pandas as pd  # Python Data Analysis Library
+"""Statistical Programming in Python."""
+
 import os  # Miscellaneous operating system interfaces
 from pathlib import Path  # Object-oriented filesystem paths
+
+import numpy as np  # Fundamental package for scientific computing with Python
+import pandas as pd  # Python Data Analysis Library
+import seaborn as sns  # Statistical data visualization
+from matplotlib import pyplot as plt  # Visualization with Python
+from sqlalchemy import create_engine  # sqlalchemy engine
+
 print("Hello World")
 # -------------------------- Installing and Preparation -----------------------
 # Check Python Version -> In eTerminal
@@ -49,10 +53,10 @@ plt.figure(figsize=(8, 8), dpi=100)
 fig1 = sns.boxplot(y=df["sepal_length"])
 plt.savefig(os.path.join(figures_path, 'fig1.pdf'), format='pdf')
 # --------------------------- Assignment and Operations ----------------------
-weight = 72.0  # weight in kilograms
-height = 1.82  # height in meters
-BMI = weight/(height**2)  # formula to calculate Body Mass Index (kg/m^2)
-BMI
+MYWEIGHT = 72.0  # weight in kilograms
+MYHEIGHT = 1.82  # height in meters
+BMI = MYWEIGHT/(MYHEIGHT**2)  # formula to calculate Body Mass Index (kg/m^2)
+print(BMI)
 
 values1 = list(range(1, 101))  # create a list
 print(values1)  # print the list values
@@ -66,32 +70,33 @@ np.nanmean(values2)  # find the mean of the array and remove nan
 # ---------------------------- Functions and Conditions -----------------------
 
 
-def BodyMassIndex(weight, height):
+def body_mass_index(weight, height):
+    """Body mass index."""
     if height > 2.5:
         raise ValueError('height is not in meters')
     else:
-        return(weight/(height**2))
+        return weight/(height**2)
 
 
-BodyMassIndex(72, 1.82)
+body_mass_index(72, 1.82)
 # ----------------------------- Data Types and Classes ------------------------
 # -------------------------------- scalar variables ---------------------------
-# a float variable is a numeric with fractional partts
-float_var = 8.4
-type(float_var)
-isinstance(float_var, float)
-# an ineteger is Positive or negative whole number (without a fractional part)
-int_var = 6
-type(int_var)
-isinstance(int_var, int)
+# a float variable is a numeric with fractional parts
+FLOAT_VAR = 8.4
+type(FLOAT_VAR)
+isinstance(FLOAT_VAR, float)
+# an integer is Positive or negative whole number (without a fractional part)
+INT_VAR = 6
+type(INT_VAR)
+isinstance(INT_VAR, int)
 # a string variable is a collection of one or more characters
-str_var = "foo"
-type(str_var)
-isinstance(str_var, str)
+STR_VAR = "foo"
+type(STR_VAR)
+isinstance(STR_VAR, str)
 # a boolean variable is composed of True/False data
-bool_var = True
-type(bool_var)
-isinstance(bool_var, bool)
+BOOL_VAR = True
+type(BOOL_VAR)
+isinstance(BOOL_VAR, bool)
 # ---------------------------- Dictionaries and Lists -------------------------
 # a dictionary is an unordered collection key:value pair of data
 dict_var = {1: "a", 2: "b", 3: "c", 4: "d"}
@@ -109,21 +114,21 @@ isinstance(tuple_var, tuple)
 # -------------------------------- Numpy Arrays -------------------------------
 
 float_array = np.array([1, 2.5, 4.5])
-float_array.dtype.name
+print(float_array.dtype.name)
 isinstance(float_array[0], float)
 isinstance(float_array[0], np.float_)
 isinstance(float_array[0], np.double)
 
-str_array = np.array(['1', 'a', 4.5])  # notice type coersion
-str_array.dtype.name
+str_array = np.array(['1', 'a', 4.5])  # notice type coercion
+print(str_array.dtype.name)
 isinstance(str_array[0], np.str_)
 
 int_array = np.array([1, 6, 10])
-int_array.dtype.name
+print(int_array.dtype.name)
 isinstance(int_array[0], np.int_)
 
 log_array = np.array([True, False, True, False])
-log_array.dtype.name
+print(log_array.dtype.name)
 isinstance(log_array[0], np.bool)
 isinstance(log_array[0], np.bool8)
 isinstance(log_array[0], np.bool_)
@@ -135,35 +140,35 @@ df = pd.DataFrame({
     'gender': pd.Categorical(["m", "f", "m"]),
     'fbs': np.array([104, 98, 129])
 })
-df.shape  # dimensions of a dataframe
-df.dtypes  # data types of variables in a dataframe
-df.head()  # view just a head of dataframe
-df.describe()  # describe dataframe
-df.columns  # shows the column names of datafram
-df.T  # transpose
+print(df.shape)  # dimensions of a dataframe
+print(df.dtypes)  # data types of variables in a dataframe
+print(df.head())  # view just a head of dataframe
+print(df.describe())  # describe dataframe
+print(df.columns)  # shows the column names of dataframe
+print(df.T)  # transpose
 type(df)  # shows the type of a dataframe
 # check if an object is of class pandas dataframe
 isinstance(df, pd.core.frame.DataFrame)
 # ------------------------ Selection/Subsetting DataFrames --------------------
-df.columns[0]  # shows the name of the first column of a dataframe
-df['id']  # selecting a single column
-df[:1]  # selecting a single row
+print(df.columns[0])  # shows the name of the first column of a dataframe
+print(df['id'])  # selecting a single column
+print(df[:1])  # selecting a single row
 # select via the label
-df.loc[:, ['gender', 'fbs']]  # selecting on a multi-axis by label
-df.loc['0':'1', ['id', 'gender']]  # showing label slicing
-df.at[0, 'fbs']
+print(df.loc[:, ['gender', 'fbs']])  # selecting on a multi-axis by label
+print(df.loc['0':'1', ['id', 'gender']])  # showing label slicing
+print(df.at[0, 'fbs'])
 # select via the position of the passed integers
-df.iloc[2]  # select data of row with index 2, i.e., 3rd row
-df.iloc[0:2, 0:2]  # select by integer slices
-df.iloc[1:3, :]  # slicing rows explicitly
-df.iloc[:, 0:2]  # slicing columns explicitly
-df.iloc[1, 1]  # getting a value explicitly
+print(df.iloc[2])  # select data of row with index 2, i.e., 3rd row
+print(df.iloc[0:2, 0:2])  # select by integer slices
+print(df.iloc[1:3, :])  # slicing rows explicitly
+print(df.iloc[:, 0:2])  # slicing columns explicitly
+print(df.iloc[1, 1])  # getting a value explicitly
 # select via Boolean indexing i.e. conditions
-df[df['fbs'] > 100]  # using subsetting approach 
+print(df[df['fbs'] > 100])  # using subsetting approach
 df.query('fbs > 100')  # using query approach
 df2 = df.copy()  # make a copy of dataframe for further handlings
 df2.loc[3] = [4, 'other', np.nan]  # add another row to dataframe
-df2[df2['gender'].isin(['m', 'f'])]  # using the isin() method for filtering
+print(df2[df2['gender'].isin(['m', 'f'])])  # using the isin() method for filtering
 pd.isna(df2)  # To get the boolean mask where values are nan (i.e., missing)
 df2.dropna(how='any')  # To drop any rows that have missing data.
 # ---------------------------- Operations on DataFrames ------------------------
@@ -179,7 +184,7 @@ df.apply(np.cumsum)
 df3 = pd.DataFrame(np.random.randn(10, 4))
 pieces = [df3[:3], df3[3:7], df3[7:]]
 df3_concat = pd.concat(pieces)
-df3_concat == df3
+print(df3_concat == df3)
 # join data frames by SQL style merges
 left = pd.DataFrame({'key': ['foo', 'bar'], 'lval': [1, 2]})
 right = pd.DataFrame({'key': ['foo', 'bar'], 'rval': [4, 5]})
@@ -198,7 +203,7 @@ df4.groupby(['A', 'B']).size()
 df4.groupby('A').agg({'D': 'mean'}) # aggregate method
 # mutating/assigning new variables
 df4 = df4.assign(E = df4['D'] - df4['C'])
-# renaming column/variable names 
+# renaming column/variable names
 df4 = df4.rename(columns = {'E': 'Subtract'})
 # pivot tables
 pd.pivot_table(df4, values='D', index=['A'], columns=['B'])
@@ -209,11 +214,11 @@ df4.sort_values(by="C")
 df5 = pd.read_csv(
     os.path.join(data_path, 'df.csv')
 )
-df5.shape  # dimensions of a dataframe
-df5.dtypes  # data types of variables in a dataframe
-df5.head()  # view just a head of dataframe
-df5.describe()  # describe dataframe
-df5['gender'].nunique()  # get the unique number of categries
+print(df5.shape)  # dimensions of a dataframe
+print(df5.dtypes)  # data types of variables in a dataframe
+print(df5.head())  # view just a head of dataframe
+print(df5.describe())  # describe dataframe
+df5['gender'].nunique()  # get the unique number of categories
 # writing to csv
 df5.to_csv(
     os.path.join(data_path, 'df.csv')
@@ -221,7 +226,7 @@ df5.to_csv(
 # writing to a HDF5 store (Apache Hadoop Distributed File System)
 df5.to_hdf(
     path_or_buf=os.path.join(data_path, 'df5.h5'),
-    key='df5', 
+    key='df5',
     mode='w'
     )
 # reading from HDF5
@@ -235,7 +240,6 @@ df7 = pd.read_excel(
 )
 all(df7 == df6)
 
-from sqlalchemy import create_engine
 # https://medium.com/@sara.khorram/inserting-python-dataframe-into-sql-table-increasing-the-speed-760a33db5ab5
 method_6_insert_time_list = []
 engine = create_engine(
